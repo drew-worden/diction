3818ca330eca607df264337a39800d87149e637f
drew-worden <drew.p.worden@gmail.com>
2024-01-06 21:06:09 -0500

    Added loader component.

---
+++ client/src/components/Loader.svelte
@@ -0,0 +1,51 @@
+<script>
+    // Props
+    export let width = "200px";
+</script>
+
+<div class="loader" style={`width:${width};`}></div>
+
+<style>
+.loader {
+  display: block;
+  --height-of-loader: 8px;
+  --loader-color: #54a6ee;
+  height: var(--height-of-loader);
+  border-radius: 30px;
+  background-color: rgba(0,0,0,0.2);
+  position: relative;
+  box-shadow:
+			rgba(0, 0, 0, 0.4) 0px 2px 4px,
+			rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
+			rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+}
+
+.loader::before {
+  content: "";
+  position: absolute;
+  background: var(--loader-color);
+  top: 0;
+  left: 0;
+  width: 0%;
+  height: 100%;
+  border-radius: 30px;
+  animation: moving 1s ease-in-out infinite;
+  box-shadow:
+			rgba(0, 0, 0, 0.4) 0px 2px 4px,
+			rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
+			rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+}
+
+@keyframes moving {
+  50% {
+    width: 100%;
+  }
+
+  100% {
+    width: 0;
+    right: 0;
+    left: unset;
+  }
+}
+
+</style>
